fb5eded3d85bfa9be6b237dba42e31d92aebfac2
Prashant Srivastava <prashant.k.srivastava29@gmail.com>
2022-04-26 00:54:57 +0530

    Created a graph library

---
+++ ConsoleApp1/DS.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Graphs;
+
+Console.WriteLine("Hello, World!");
+List<List<int>> graph = Graph.CreateGraph(5);
+Graph.AddEdge(graph, 0, 1);
+Graph.AddEdge(graph, 0, 2);
+Graph.AddEdge(graph, 1, 2);
+Graph.AddEdge(graph, 1, 3);
+Graph.AddEdge(graph, 4, 1);
+Graph.PrintGraph(graph);
---
+++ Graphs/Graph.cs
@@ -0,0 +1,45 @@
+﻿namespace Graphs
+{
+    /// <summary>
+    ///   The Graph class for creating an un-dected graph and performing its operations
+    /// </summary>
+    public static class Graph
+    {
+        /// <summary>
+        /// Creates the graph.
+        /// </summary>
+        /// <param name="v">Number of vertices.</param>
+        /// <returns></returns>
+        public static List<List<int>> CreateGraph(int v)
+        {
+            List<List<int>> adj = new List<List<int>>(v);
+            for(int i=0;i<v;i++)
+                adj.Add(new List<int>());
+            return adj;
+        }
+        /// <summary>
+        /// Adds the edge.
+        /// </summary>
+        /// <param name="adj">The adjacency list representation of a Graph.</param>
+        /// <param name="u">From vertex.</param>
+        /// <param name="v">To vertex.</param>
+        public static void AddEdge(List<List<int>> adj, int u, int v)
+        {
+            adj[u].Add(v);
+            adj[v].Add(u);
+        }
+        /// <summary>
+        /// Prints the graph.
+        /// </summary>
+        /// <param name="adj">The adjacency list representation of a Graph.</param>
+        public static void PrintGraph(List<List<int>> adj)
+        {
+            for (int i = 0; i < adj.Count; i++)
+            {
+                for (int j = 0; j < adj[i].Count; j++)
+                    Console.Write(adj[i][j]+" ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
